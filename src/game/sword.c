5c2eed2d8e8de85ec8a6c3b55e3824a2094d06fd
anisdjl <adjelili954@gmail.com>
2026-05-21 17:41:43 +0200

    cub3d

--- src/game/sword.c
+++ src/game/sword.c
@@ -1,8 +1,17 @@
 #include "../../header/graphics.h"
 
-// void	put_sword(t_graphics **graph)
-// {
-// 	int	x;
+void	put_sword(t_graphics **graph)
+{
+	int	x;
+	int	y;
 
-// 	x = (WIDTH / 2) - 
-// }
+	x = (WIDTH / 2) - ((*graph)->width_sword / 2);
+	y = HEIGHT - (*graph)->height_sword;
+	while (x < (*graph)->width_sword)
+	{
+		while (y < HEIGHT)
+		{
+			if ()
+		}
+	}
+}
